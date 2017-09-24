2deca53b3d7503c8aa449b03b9dc6ba4da3cd4f0
Laura A. Miller <lauramil@andrew.cmu.edu>
2017-09-24 16:00:43 -0400

    Made a split screen camera and got one of the cameras to follow the player ;

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+	public GameObject person;
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+
+		offset = transform.position - person.transform.position;
+		
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+
+		transform.position = person.transform.position + offset;
+	}
+}
