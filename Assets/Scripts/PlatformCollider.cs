01c51cf24550826436295eefa8179d6cbeb6d760
Nathan Blinn <nathan@nathanblinn.com>
2017-09-29 16:02:57 -0400

    Improve platform detection

--- Assets/Scripts/PlatformCollider.cs
+++ Assets/Scripts/PlatformCollider.cs
@@ -19,9 +19,7 @@ public class PlatformCollider : MonoBehaviour
     private void FixedUpdate()
     {
         CheckCollisions(human);
-        //CheckCollisions(shadow);
-
-        
+        //CheckCollisions(shadow);        
     }
 
     private void CheckCollisions(GameObject player)
@@ -31,30 +29,40 @@ public class PlatformCollider : MonoBehaviour
 
         // Calculate the top of the player and the bottom of the platform.
         float playerHeight = playerColl.bounds.size.y;
-        float playerY = player.transform.position.y + playerHeight / 2 + 0.01f;
+        float playerTop = player.transform.position.y + playerHeight / 2 + 0.01f;
+        float playerBot = player.transform.position.y - playerHeight / 2 - 0.01f;
 
         float platformHeight = coll.bounds.size.y;
-        float platformY = transform.position.y - platformHeight / 2 - 0.01f;
+        float platformTop = transform.position.y + platformHeight / 2 + 0.01f;
+        float platformBot = transform.position.y - platformHeight / 2 - 0.01f;
+
+        bool drop = false;
                 
-        // If the player is below the platform, the player stops dropping
-        if (playerY < platformY)
+        // If player drops all the way through platform, stop dropping.
+        if (playerTop < platformBot)
         {
+            // If the player is not dropping, then drop will remain false.
             player.GetComponent<PlayerMovement>().dropping = false;
+        }
+        // If player is below the platform, but not all the way through.
+        else if (playerBot < transform.position.y)
+        {
+            drop = true;
         }
 
         // If the player is moving upwards, don't collide
         if (playerRigidbody.velocity.y > 0.1f)
         {
-            Physics2D.IgnoreCollision(coll, playerColl, true);
+            drop = true;
         }
-        //If the player is dropping through a platform, don't collide
+        //If the player is dropping through a platform, don't collide.
         else if (player.GetComponent<PlayerMovement>().dropping)
         {
-            Physics2D.IgnoreCollision(coll, playerColl, true);
+            drop = true;
         }
-        else
-        {
-            Physics2D.IgnoreCollision(coll, playerColl, false);
-        }
+
+        // If drop was never turned to true above, then collide.
+        // If drop was turned to true above, don't collide.
+        Physics2D.IgnoreCollision(coll, playerColl, drop);
     }
 }
